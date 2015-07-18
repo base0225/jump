5d36c5b6f3d7b1228172f1ef56b65ca8615d3cb7
base0225 <736952542@qq.com>
2015-07-18 17:03:47 +0800

    心跳

--- 0928ZJ心跳/ViewController.m
+++ 0928ZJ心跳/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *jump;
 
 @end
 
@@ -17,6 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //添加动画
+    CABasicAnimation *anm = [CABasicAnimation animation];
+    //设置属性值
+    anm.keyPath = @"transform.scale";
+    anm.toValue = @0;
+    //想要执行多次动画
+    anm.repeatCount = MAXFLOAT;
+//    设置动画的执行时长
+    anm.duration = 0.5;
+//    反转，怎么回去，怎么回来
+    anm.autoreverses = YES;
+//    添加动画
+    [self.jump.layer addAnimation:anm forKey:nil];
 }
 
 - (void)didReceiveMemoryWarning {
