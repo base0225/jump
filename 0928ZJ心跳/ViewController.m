de8d5ce9e51dd6e487a3fe68fe65f2d393e03a0d
base <base@basedemacbook-pro.local>
2015-09-28 21:50:12 +0800

    Initial Commit

---
+++ 0928ZJ心跳/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  0928ZJ心跳
+//
+//  Created by base on 15/09/19.
+//  Copyright © 2015年 base. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 0928ZJ心跳Tests/_928ZJ__Tests.m
@@ -0,0 +1,39 @@
+//
+//  _928ZJ__Tests.m
+//  0928ZJ心跳Tests
+//
+//  Created by base on 15/09/19.
+//  Copyright © 2015年 base. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _928ZJ__Tests : XCTestCase
+
+@end
+
+@implementation _928ZJ__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
